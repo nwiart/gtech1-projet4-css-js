41d3df6a888d5f32a3b253c69c8c19245cdb5757
nwiart <nwiart@gaming.tech>
2021-11-17 14:01:01 +0100

    Added basic project presentation page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+// Roll-A-Ball screenshot descriptions for carousel.
+rb_desc = [
+    "Splashscreen",
+    "The game's main menu",
+    "Level select menu, with a simplified 3D representation of the selected level.",
+    "The floating island level",
+    "Speed display turning red and field of view increasing at high speeds."
+];
+
 $(document).ready(function() {
     $('.sidenav').sidenav();
+    $('.carousel').carousel({
+        dist: 0,
+        padding: 0,
+        fullWidth: true,
+        indicators: true,
+        onCycleTo: function(data) {
+            $('.rbdesc').html(rb_desc[$(data).index()]);
+        }
+    });
 });
