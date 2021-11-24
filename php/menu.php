1d08e62e62981bc698cdf0360308a037696298ec
nwiart <nwiart@gaming.tech>
2021-11-24 16:06:42 +0100

    Added sign up form

--- php/menu.php
+++ php/menu.php
@@ -10,7 +10,7 @@
 			<li><a href="rb.php" class="btn waves-effect waves-light red darken-2">Roll-A-Ball!</a></li>
 		</ul>
 		<ul class="left hide-on-med-and-down">
-			<li><a href="rb.php" class="btn waves-effect waves-light">Sign Up</a></li>
+			<li><a href="#modal-signup" class="btn waves-effect waves-light modal-trigger">Sign Up</a></li>
 			<li><a href="#modal-signin" class="btn waves-effect waves-light modal-trigger">Sign In</a></li>
 		</ul>
 	</div>
@@ -21,12 +21,13 @@
 	<li><a href="ug-editor.php" class="btn red darken-2">UGE Editor</a></li>
 	<li><a href="rb.php" class="btn red darken-2">Roll-A-Ball!</a></li>
 
-	<li><a href="rb.php" class="btn waves-effect waves-light">Sign Up</a></li>
-	<li><a href="rb.php" class="btn waves-effect waves-light">Sign In</a></li>
+	<li><a href="#modal-signup" class="btn waves-effect waves-light modal-trigger">Sign Up</a></li>
+	<li><a href="#modal-signin" class="btn waves-effect waves-light modal-trigger">Sign In</a></li>
 </ul>
 
 
 
+<!-- Sign In form. -->
 <div id="modal-signin" class="modal">
     <div class="modal-content">
         <h3>Sign In</h3>
@@ -48,7 +49,6 @@
                         <i class="material-icons prefix red-text text-darken-2">account_circle</i>
                         <input name="login" id="login" type="text" required="" aria-required="true" />
                         <label for="login">Login</label>
-                        <span class="helper-text" data-error="wrong" data-success="right"></span>
                     </div>
 
 					<!-- Password. -->
@@ -60,6 +60,58 @@
 
                     <div class="center">
                         <button type="submit" class="btn waves-effect waves-light red darken-2">Sign In</button>
+                    </div>
+                </div>
+                <div class="col l2 m1 hide-on-small-only"></div>
+
+            </div>
+        </form>
+    </div>
+    <div class="modal-footer">
+        <a href="#!" class="modal-close btn waves-effect waves-green red darken-2">Close</a>
+    </div>
+</div>
+
+<!-- Sign Up form. -->
+<div id="modal-signup" class="modal">
+    <div class="modal-content">
+        <h3>Sign Up</h3>
+        <p>
+            Please fill in the form below if you wish to send us a message. <br />
+            Note : Be on the lookout for any strange detail on the main page. If you find it, be sure to come back here!
+        </p>
+
+        <!-- Contact form. -->
+        <form method="post" action="signup.php" class="col s12">
+            <div class="row" id="contact-block">
+                
+                <!-- Form layout. A certain number of columns are taken on either side depending on the screen width. -->
+                <div class="col l2 m1 hide-on-small-only"></div>
+                <div class="col s12 m10 l8">
+
+					<!-- Login. -->
+                    <div class="input-field">
+                        <i class="material-icons prefix red-text text-darken-2">account_circle</i>
+                        <input name="login" id="login" type="text" required="" aria-required="true" />
+                        <label for="login">Login</label>
+                    </div>
+
+					<!-- Email. -->
+                    <div class="input-field">
+                        <i class="material-icons prefix red-text text-darken-2">email</i>
+                        <input name="email" id="email" type="email" required="" aria-required="true" />
+                        <label for="email">Email</label>
+                    </div>
+
+					<!-- Password. -->
+                    <div class="input-field">
+                        <i class="material-icons prefix red-text text-darken-2">key</i>
+                        <input name="password" id="password" type="password" required="" aria-required="true" />
+                        <label for="password">Password</label>
+                    </div>
+
+                    <div class="center">
+                        <button type="submit" class="btn waves-effect waves-light red darken-2">Sign Up</button>
                     </div>
                 </div>
                 <div class="col l2 m1 hide-on-small-only"></div>
