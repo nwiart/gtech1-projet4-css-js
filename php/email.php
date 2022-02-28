03d33bbc96548ac2cba47cdb92c53a1ec89cb490
nwiart <nwiart@gaming.tech>
2022-02-28 22:34:02 +0100

    Email send didn't redirect user

--- php/email.php
+++ php/email.php
@@ -3,4 +3,5 @@
     $message_email = $_POST["first-name"] . " " . $_POST["last-name"] . ", qui a comme adresse mail " . $_POST["email"] . " vous a envoyé un message :\n\n" . $_POST["message"];
     mail("ejoachimgabin@gaming.tech", "Contact us from " . $_POST["first-name"], $message_email);
 
+    header("Location: ../index.php");
 ?>
