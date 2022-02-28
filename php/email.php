d5cc8988a36a285e258d41128091aabd0b17f5c6
nwiart <nwiart@gaming.tech>
2022-02-28 22:39:10 +0100

    Refixed email.php

--- php/email.php
+++ php/email.php
@@ -1,7 +1,7 @@
 <?php
+        $message_email = $_POST["first-name"] . " " . $_POST["last-name"] . ", qui a comme adresse mail " . $_POST["email"] . " vous a envoyé un message :\n\n" . $_POST["message"];
+        mail("ejoachimgabin@gaming.tech", "Contact us from " . $_POST["first-name"], $message_email);
+        mail("nwiart@gaming.tech", "Contact us from " . $_POST["first-name"], $message_email);
 
-    $message_email = $_POST["first-name"] . " " . $_POST["last-name"] . ", qui a comme adresse mail " . $_POST["email"] . " vous a envoyé un message :\n\n" . $_POST["message"];
-    mail("ejoachimgabin@gaming.tech", "Contact us from " . $_POST["first-name"], $message_email);
-
-    header("Location: ../index.php");
+        header("Location: ../index.php");
 ?>
